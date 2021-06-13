e864583868512f3097b160f9971ab62c87edeb89
yanxinbo <yanxinbo@51shebao.com>
2021-06-13 16:22:47 +0800

    vnode的传递和渲染

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,14 +1,10 @@
 <template>
-  <div>
-    heheh
-    <slot>
-      <div v-html="list"></div>
-    </slot>
-  </div>
 
-  <!-- <div>
-    <Bar name="zhangsan" msg="this is a text msg"></Bar>
-  </div>-->
+  <div>
+    <Bar name="zhangsan"
+         :list="list"
+         msg="this is a text msg"></Bar>
+  </div>
 
   <!-- <router-link to="/">go to home</router-link> -->
   <!-- <p>this page about</p> -->
@@ -23,53 +19,54 @@
 </template>
 
 <script   lang="ts">
-import { renderList, h, defineComponent } from "vue"
-import { Bar } from "./testJsx";
-import Layout from "./layout";
-import { debug } from "console";
+  import { renderList, h, defineComponent } from "vue";
+  import { Bar } from "./testJsx";
+  import Layout from "./layout";
+  import { debug } from "console";
+  const list = h("p", "this is a slot msg  from parent");
 
-// const list = renderList([1, 2, 3, 4], (item, index) => h('p', { key: 'item' }, item))
-export default defineComponent({
+  // const list = renderList([1, 2, 3, 4], (item, index) => h('p', { key: 'item' }, item))
+  export default defineComponent({
+    components: {
+      Bar,
+      Layout,
+    },
 
-  components: {
-    Bar,
-    Layout
-  },
+    data() {
+      return {
+        list: list,
+      };
+    },
 
-  setup() {
-    // TODO   探究模版渲染方式 ？
-    // TODO   vhtml ???
-    const list = h('p', 'cccc')
+    setup() {
+      // TODO   探究模版渲染方式 ？
+      // TODO   vhtml ???
 
-    return {
-      list
-    }
+      return {
+        // list,
+      };
 
+      //返回 function 时 ,和render合并   并优先解析render函数
+      // 所以 tempalte中的内容不会解析
+      // return () => list
+    },
+  });
 
-    //返回 function 时 ,和render合并   并优先解析render函数
-    // 所以 tempalte中的内容不会解析
-    // return () => list
-  }
-})
+  // script setup  中可用
 
+  // import style from "../styles/style.module.scss";
+  // import { useStore } from "vuex";
+  // const store = useStore();
+  // const test = style.background;
 
-// script setup  中可用
+  // const props = defineProps({
+  //   msg: String
+  // })
 
-
-// import style from "../styles/style.module.scss";
-// import { useStore } from "vuex";
-// const store = useStore();
-// const test = style.background;
-
-
-// const props = defineProps({
-//   msg: String
-// })
-
-// const addHandle = () => {
-//   store.commit("increment");
-// };
-// const deHadlde = () => {
-//   store.commit("decrement");
-// };
+  // const addHandle = () => {
+  //   store.commit("increment");
+  // };
+  // const deHadlde = () => {
+  //   store.commit("decrement");
+  // };
 </script>
--- src/components/testJsx.tsx
+++ src/components/testJsx.tsx
@@ -6,6 +6,10 @@ import { defineComponent, toRefs, renderList, h } from 'vue'
 const Bar = defineComponent({
   name: 'testSlots',
   props: {
+    list: {
+      type: Object
+    },
+
     msg: {
       type: String,
       required: true
@@ -18,16 +22,27 @@ const Bar = defineComponent({
 
   setup (props, { attrs, slots, emit, expose }) {
 
-    const list = renderList([1, 2, 3, 4, 5, 6], item => h(`h${item}`, item))
-    const { msg } = toRefs(props)
+
+    const testList = renderList([1, 2, 3, 4, 5, 6], item => h(`h${item}`, item))
+    // const { msg, list } = toRefs(props)
+
+
+    const list2 = h("p", "this is a slot msg  from parent")
+
     // const msg = toRef(attrs,'msg')
     return () => (
       <>
-        <p>{msg.value}</p>
+        {/* <p>{msg.value}</p> */}
         {/* {(slots as InternalSlots).header()} */}
         {/* {slots.header()} */}
-        <p innerHTML={msg.value + 'ccccc'}></p>
-        <div> {list} </div>
+        {/* <p innerHTML={msg.value + 'ccccc'}></p> */}
+        <div> {testList} </div>
+        {/* TODO */}
+        {/* 不能用  toRefs   */}
+        {/* 不能用  toRefs   */}
+        {/* 不能用  toRefs   */}
+        <div> list  {props.list} </div>
+        <div> list2 {list2} </div>
       </>
     )
   },
