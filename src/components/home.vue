deed2245243968d570aa37f85c8c4dad2a2cb5ff
yanxinbo <yanxinbo@51shebao.com>
2021-07-14 17:56:28 +0800

    merge

--- src/components/home.vue
+++ src/components/home.vue
@@ -15,27 +15,6 @@
 </template>
 
 <script setup lang="ts">
-<<<<<<< HEAD
-// import layout from "./layout"
-import { ref, reactive, defineComponent, defineProps, } from "vue"
-import { getList } from "../axios"
-const list = reactive<Array<object>>([])
-const columns = [
-  { title: "许序号", dataIndex: "key", key: "key" },
-  { title: "ID", dataIndex: "id", key: "id" },
-  { title: "子产品", dataIndex: "childProductName", key: "childProductName" },
-]
-getList()
-  .then((result) => {
-    list.push(
-      ...result.data.data.list.map((item: object, index: number) => ({
-        ...item,
-        key: index,
-      }))
-    )
-  })
-  .catch((err) => { })
-=======
   import { useStore } from "vuex"
   import layout from "./layout"
   import { ref, reactive, defineComponent, defineProps, computed } from "vue"
@@ -54,8 +33,6 @@ getList()
     })
     .catch((err) => { })
     .finally(() => { })
->>>>>>> 4a858eb8e9ff8699a29effa9600558abf4669fc0
-
   // const props = defineProps({
   //   msg: String
   // })
